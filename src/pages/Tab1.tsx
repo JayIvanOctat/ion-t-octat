28220b879d9afd90aef1b2bc1ddb1e41acf8fcec
JayIvanOctat <reazon07@gmail.com>
2024-03-19 17:32:22 +0800

    added changes to tab1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -4,7 +4,10 @@ import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,I
 function Example() {
   return (
     <IonPage>
-    <IonHeader>
+      <IonHeader>
+          <IonToolbar>
+            <IonTitle>Profile</IonTitle>
+          </IonToolbar>
     <IonCard>
       <img alt="Grass" src="https://imgs.search.brave.com/ILsDnp_oP9XlSTqoTpObMpnxwFG0ska89WWwmiQ8Gr0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/MzM1NjM5MDYwOTEt/ZmRmZGZmYzNlM2M0/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4TVRSOGZH/UmhjbXNsTWpCbmNt/VmxibnhsYm53d2ZI/d3dmSHg4TUE9PQ.jpeg" />
       <IonCardHeader>
