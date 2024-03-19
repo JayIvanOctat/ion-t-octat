79d05953733408174dd184fb20497ef89d30b696
JayIvanOctat <reazon07@gmail.com>
2024-03-19 16:48:43 +0800

    added content on homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,29 +1,28 @@
-import {
-    //Initial Components
-    IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonTabButton
-} from '@ionic/react';
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 
-// CSS
-//import './profile.css';
-  
-  const Home: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle>Home</IonTitle>
+            <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent fullscreen>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Home</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  //
-  export default Home;
+        <div className="ion-padding">
+          <h1>Welcome to Home Page</h1>
+          <p>This is a sample content for the home page.</p>
+          <IonButton expand="block" routerLink="/profile">Go to Profile</IonButton>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
